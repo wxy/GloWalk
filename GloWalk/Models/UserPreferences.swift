27264c3756bda817ae9e45a1ca6dae0d0c5c7ff2
Xingyu Wang <xingyu.wang@gmail.com>
2026-07-23 21:59:33 +0800

    chore: remove unused settings (defaultBrightness, WiFi auto-arrival, SSID list)

--- GloWalk/Models/UserPreferences.swift
+++ GloWalk/Models/UserPreferences.swift
@@ -1,9 +1,6 @@
 import SwiftUI
 
 final class UserPreferences: ObservableObject {
-    @AppStorage("defaultBrightness") var defaultBrightness: Double = 0.7
-    @AppStorage("enableWiFiArrival") var enableWiFiArrival: Bool = false
-    @AppStorage("arrivalWiFiSSIDs") var arrivalWiFiSSIDs: String = ""
     @AppStorage("language") var language: String = "system"
 
     static let shared = UserPreferences()
